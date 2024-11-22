c3ca715358d3c97c6db48a4b345462d1678a50c9
FelipeAlvarado01 <afvalvarado@gmail.com>
2024-11-22 17:47:34 -0500

    eventos de click,listas y llamado de componentes en otros componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import Card, { CardBody } from "./components/Card";
+import List from "./components/List";
+
 function App() {
-  return <p>Hola Mundo</p>;
+  const list = ["Juan", "Jorge", "Joah"];
+  return (
+    <Card>
+      <CardBody title="Hola Mundo" text="Esto es para el texto" />
+      <List data={list} />
+    </Card>
+  );
 }
 
 export default App;
---
+++ src/components/List.tsx
@@ -0,0 +1,26 @@
+import { MouseEvent } from "react";
+
+type Props = {
+  data: string[];
+};
+
+function List({ data }: Props) {
+  const handleClick = (event: string) => {
+    console.log(event);
+  };
+  return (
+    <ul className="list-group">
+      {data.map((elemento) => (
+        <li
+          onClick={() => handleClick(elemento)}
+          key={elemento}
+          className="list-group-item"
+        >
+          {elemento}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default List;
